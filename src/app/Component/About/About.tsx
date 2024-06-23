b4b81dfe90b74357d7b77dd1f899b7a5834f0f1c
Md Arif <arif.20527@sscbs.du.ac.in>
2024-06-24 00:38:38 +0530

    added alt prop to skillcard

--- src/app/Component/About/About.tsx
+++ src/app/Component/About/About.tsx
@@ -9,13 +9,13 @@ const About = () => {
         <h2 style={{fontSize:"40px"}}>About me</h2>
         <h3>Full Stack Developer</h3>
         <p>
-            I'm Md Arif and I am from Bhadohi,Uttar Pradesh. 
+            I am Md Arif and I am from Bhadohi,Uttar Pradesh. 
             I am a Computer Science Graduate from University of Delhi.
              I have worked hard in my graduation and scored 8.75 CGPA.
               Along with my Degree I prepared for Nimcet 2023 and also 
               got AIR 487. Along with my degree i learnt C++, Java, Python,
                SQL. Currently am in National Institute of Technology Jamshedpur, 
-               pursuing my master's(MCA) and exploring technologies like Web Development,
+               pursuing my masters(MCA) and exploring technologies like Web Development,
                 Machine Learning, Data Structures and Algorithms, various programming languages.
                 Loooking forward to the journey ahead...
         </p>
--- src/app/Component/Skills/SkillCard.tsx
+++ src/app/Component/Skills/SkillCard.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from './Skill.module.css'
 const SkillCard = (props:any) => {
   return (
-    <div className={styles.skillCard}><Image {...props} width={80} height={80}></Image>
+    <div className={styles.skillCard}><Image src={props.src} alt={props.alt} width={80} height={80}></Image>
     </div>
   )
 }
